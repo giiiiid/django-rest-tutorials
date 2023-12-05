cd1e432f6bdcee6073b5a2e331b7602984a51860
Giiid <gideonfummey1@gmail.com>
2023-12-05 02:54:04 +0100

    started app again

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import Register, Login
 
 urlpatterns = [
-    path('add/', Register.as_view(), name='add'),
-    path('login/', Login.as_view(), name='login')
+    
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,36 +1,3 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate
-from .serializers import RegisterSerializer, LoginSerializer
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import User
+
 # Create your views here.
-
-class Register(GenericAPIView):
-    # data = request.data
-    serializer_class = RegisterSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-
-        if serializer.is_valid(): 
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class Login(GenericAPIView):
-    serializer_class = LoginSerializer
-
-    def post(self, request, *args, **kwargs):
-        username = request.data.get("username")
-        password = request.data.get("password")
-
-        user = authenticate(username=username, password=password)
-
-        if user:
-            serializer = self.serializer_class(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({"message":"Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
